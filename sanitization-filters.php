0745a9c346913e7289e833a70ad1ce26676bdfc8
Sang-Wa <pcman8130@gmail.com>
2026-04-14 16:33:31 +0900

    commit

---
+++ sanitization-filters.php
@@ -0,0 +1,21 @@
+<?php // @sanitization-filters.php
+
+$user['name'] = 'Ivy<script src= js/bad.js></script>';              //user name
+$user['age'] = 23.75;                                               //user age
+$user['email'] = '💕Ivy@eg.link/';                                  //user email
+
+$senitize_user['name'] = FILTER_SANITIZE_FULL_SPECIAL_CHARS;                // HTML Escape filter
+$senitize_user['age'] = FILTER_SANITIZE_NUMBER_INT;                     // Integer filter
+$senitize_user['email'] = FILTER_SANITIZE_EMAIL;                    // Email filter
+
+$user = filter_var_array($user, $senitize_user);                        // Sanitize output
+?>
+
+<?php include 'includes/header.php'; ?>
+
+<p>Name:  <?= $user['name'] ?></p>
+<p>Age:   <?= $user['age'] ?></p>
+<p>Email: <?= $user['email'] ?></p>
+<pre><?php var_dump($user); ?></pre>
+
+<?php include 'includes/footer.php'; ?>
